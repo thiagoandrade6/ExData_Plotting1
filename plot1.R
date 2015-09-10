b7d4066ddeb231c84d82621428d16bfc7ff15de6
Thiago Pereira <thiagoandrade6@gmail.com>
2015-09-10 13:23:53 -0300

    init plot 1

---
+++ plot1.R
@@ -0,0 +1,16 @@
+plot1 <- function() {
+  
+  library(sqldf)
+  library(dplyr)
+  
+  url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+  file <- download.file(url, destfile = "Downloads/exdata.zip")
+  unzip("Downloads/exdata.zip", exdir = "Downloads/")
+  
+  csv_1_2_2007 <- read.csv.sql(file = "Downloads/household_power_consumption.txt", sql = "select * from file where Date == '1/2/2007'", header = TRUE, sep = ";")
+  csv_2_2_2007 <- read.csv.sql(file = "Downloads/household_power_consumption.txt", sql = "select * from file where Date == '2/2/2007'", header = TRUE, sep = ";")
+  df <- bind_rows(csv_1_2_2007, csv_2_2_2007)
+  hist(df$Global_active_power, main = "Global Active Power", xlab = "Global Active Power(kilowatts)", col = "red")
+  
+  #send to a PNG file
+}
