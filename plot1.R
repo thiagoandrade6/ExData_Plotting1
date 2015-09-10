1b755698db93cb5789aab2c37ccd74c84cf31422
Thiago Pereira <thiagoandrade6@gmail.com>
2015-09-10 15:28:05 -0300

    plot1.R done along with the png, also including .gitignore

--- plot1.R
+++ plot1.R
@@ -10,7 +10,9 @@ plot1 <- function() {
   csv_1_2_2007 <- read.csv.sql(file = "Downloads/household_power_consumption.txt", sql = "select * from file where Date == '1/2/2007'", header = TRUE, sep = ";")
   csv_2_2_2007 <- read.csv.sql(file = "Downloads/household_power_consumption.txt", sql = "select * from file where Date == '2/2/2007'", header = TRUE, sep = ";")
   df <- bind_rows(csv_1_2_2007, csv_2_2_2007)
+  
   hist(df$Global_active_power, main = "Global Active Power", xlab = "Global Active Power(kilowatts)", col = "red")
   
-  #send to a PNG file
+  dev.copy(png, 'plot1.png')
+  dev.off()
 }
